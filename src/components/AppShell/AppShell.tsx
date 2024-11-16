70597c9c28fa6d3a94e47e3adda77897b47b0406
kris-sprint <k.haronek@reply.com>
2024-11-16 15:43:22 +0000

    style: app shell

--- src/components/AppShell/AppShell.tsx
+++ src/components/AppShell/AppShell.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from "./AppShell.module.css"
+import React from "react";
+import { NavLink } from "react-router-dom";  // Import NavLink
+import styles from "./AppShell.module.css";
+
+import { MdHome } from "react-icons/md";
+import { BiSolidHome } from "react-icons/bi";
+import { BiSolidPlaneAlt } from "react-icons/bi";
+import { BsFillBookmarkFill } from "react-icons/bs";
+import { BsChatLeftTextFill } from "react-icons/bs";
 
 interface AppShellProps {
   children: React.ReactNode;
@@ -9,23 +15,56 @@ interface AppShellProps {
 export const AppShell: React.FC<AppShellProps> = ({ children }) => {
   return (
     <div className={styles.container}>
-
-      {/* TODO seperate component */}
+      {/* TODO separate component */}
       <header className={styles.header}>
-        <nav className={styles.nav}>
-          <div className={styles.navContent}>
-            {/* <div className={styles.logo}>My PWA</div> */}
-            <div className={styles.navLinks}>
-              <Link to="/" className={styles.navLink}>Home</Link>
-              <Link to="/flights" className={styles.navLink}>Flights</Link>
-              {/* <Link to="/profile" className={styles.navLink}>Profile</Link> */}
-            </div>
-          </div>
-        </nav>
+        <div className={styles.logo}>My PWA</div>
       </header>
 
       <main className={styles.main}>{children}</main>
 
+      <nav className={styles.nav}>
+        <div className={styles.navLinks}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? `${styles.navLinkItem} ${styles.active}` : styles.navLinkItem
+            }
+          >
+            <BiSolidHome />
+            <span>Home</span>
+          </NavLink>
+
+          <NavLink
+            to="/flights"
+            className={({ isActive }) =>
+              isActive ? `${styles.navLinkItem} ${styles.active}` : styles.navLinkItem
+            }
+          >
+            <BiSolidPlaneAlt />
+            <span>Flights</span>
+          </NavLink>
+
+          <NavLink
+            to="/tracked"
+            className={({ isActive }) =>
+              isActive ? `${styles.navLinkItem} ${styles.active}` : styles.navLinkItem
+            }
+          >
+            <BsFillBookmarkFill />
+            <span>Tracked</span>
+          </NavLink>
+
+          <NavLink
+            to="/chat"
+            className={({ isActive }) =>
+              isActive ? `${styles.navLinkItem} ${styles.active}` : styles.navLinkItem
+            }
+          >
+            <BsChatLeftTextFill />
+            <span>Chat</span>
+          </NavLink>
+        </div>
+      </nav>
     </div>
   );
 };
